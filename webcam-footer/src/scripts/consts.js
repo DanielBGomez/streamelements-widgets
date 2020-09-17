a3f6fedfe429bfd305780b40ea383d7b610f791d
Cesar Daniel Bañuelos Gómez <cbanuelosbetter.com.mx>
2020-09-17 17:19:22 -0500

    [Updates] Scripts as modules, recent events and other minor changes

---
+++ webcam-footer/src/scripts/consts.js
@@ -0,0 +1,10 @@
+// Configurations
+module.exports = {
+    DEFAULT_INTERVAL: 3000,
+    RECENT_INTERVAL: 3000,
+    TOP_INTERVAL: 4000,
+
+    ELEMENTS_LIMIT: 10,
+    
+    AVAILABLE_VISUAL_TYPES: ['bits', 'sub-gifter', 'sub', 'follow', 'donation', 'stars']
+}
