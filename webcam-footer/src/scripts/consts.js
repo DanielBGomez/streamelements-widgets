bbb47994fd203dae497123ed8778b665d98448a2
Daniel B Gomez <contact@danielbgomez.com>
2020-09-22 09:36:47 -0500

    [Refactor] Consts prepared for streamelements field values

--- webcam-footer/src/scripts/consts.js
+++ webcam-footer/src/scripts/consts.js
@@ -1,10 +1,10 @@
 // Configurations
 module.exports = {
-    DEFAULT_INTERVAL: 3000,
-    RECENT_INTERVAL: 3000,
-    TOP_INTERVAL: 4000,
+    DEFAULT_INTERVAL: (parseInt("{{DEFAULT_INTERVAL}}") || 3) * 1000,
+    RECENT_INTERVAL: (parseInt("{{RECENT_INTERVAL}}") || 3) * 1000,
+    TOP_INTERVAL: (parseInt("{{TOP_INTERVAL}}") || 4) * 1000,
 
-    ELEMENTS_LIMIT: 10,
+    ELEMENTS_LIMIT: parseInt("{{ELEMENTS_LIMIT}}") || 10,
     
     AVAILABLE_VISUAL_TYPES: ['bits', 'sub-gifter', 'sub', 'follow', 'donation', 'stars']
 }
